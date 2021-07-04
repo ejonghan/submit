0e0e64286449dd0abc5e9ebf65b3f7ad2cb8e249
JongHan Lee <62053466+ejonghan@users.noreply.github.com>
2021-07-04 21:11:12 +0900

    route another html page

--- app/main/index.py
+++ app/main/index.py
@@ -7,3 +7,13 @@ main = Blueprint('main', __name__, url_prefix='/')
 @main.route('/main', methods=['GET'])
 def index():
     return render_template('/main/index.html')
+
+
+@main.route('/submit', methods=['GET', 'POST'])
+def submit():
+    return render_template('/submit.html')
+
+
+@main.route('/list', methods=['GET'])
+def get_list():
+    return render_template('/list.html')
