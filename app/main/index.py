6cdabe8058d9bdd80d5c772df7be7ff6c7e64e60
JongHan Lee <62053466+ejonghan@users.noreply.github.com>
2021-07-11 13:09:44 +0900

    building func for delete, update

--- app/main/index.py
+++ app/main/index.py
@@ -33,10 +33,10 @@ def submit():
         return render_template('/submit.html')
 
 
-@main.route('/get_list', methods=['POST'])
-def get_list():
+@main.route('/guestbook_list', methods=['GET', 'post'])
+def guestbook_list():
 
-    if request.method == 'POST':
+    if request.method == 'GET':
 
         # database select query
         db = Database()
@@ -50,11 +50,27 @@ def get_list():
 
             view_dict[str(view[i]['id'])] = f"{view[i]['writter']}님의 게시물"
 
-        return render_template('/get_list.html', view_dict=view_dict)
+        return render_template('/guestbook_list.html', view_dict=view_dict)
+
+    elif request.method == 'POST':
+
+        # database select query
+        db = Database()
+        sql = "SELECT * FROM tellmeaboutme.list"
+        view = db.executeAll(sql)
+
+        view_dict = {}
+
+        # create dict using script
+        for i in range(len(view)):
+
+            view_dict[str(view[i]['id'])] = f"{view[i]['writter']}님의 게시물"
+
+        return render_template('/guestbook_list.html', view_dict=view_dict)
 
 
-@main.route('/visit', methods=['GET', 'POST'])
-def visit():
+@main.route('/guestbook', methods=['GET', 'POST'])
+def guestbook():
 
     # routed when you click '보기' button
     if request.method == 'POST':
@@ -66,7 +82,7 @@ def visit():
         sql = f"SELECT * FROM tellmeaboutme.list WHERE id={get_id}"
         view = db.executeOne(sql)
 
-        return render_template('/visit.html', view=view)
+        return render_template('/guestbook.html', view=view, get_id=get_id)
 
     # routed when you clink anchor tag
     else:
@@ -76,11 +92,58 @@ def visit():
         db = Database()
         sql = f"SELECT * FROM tellmeaboutme.list WHERE id={get_id}"
         view = db.executeOne(sql)
+        print(get_id)
 
-        return render_template('/visit.html', view=view)
+        return render_template('/guestbook.html', view=view, get_id=get_id)
 
 
-@main.route('/visit/<int:id>', methods=['GET'])
-def anchor_routing_visit(id):
+@main.route('/guestbook/<int:id>', methods=['GET'])
+def anchor_routing_guestbook(id):
 
-    return redirect(url_for('main.visit', id=id))
+    return redirect(url_for('main.guestbook', id=id))
+
+
+@main.route('/delete', methods=["GET"])
+def delete():
+
+    get_id = request.args.get('id')
+
+    db = Database()
+    # database delete query
+    delete_sql = f"DELETE FROM tellmeaboutme.list WHERE id={get_id}"
+    db.execute(delete_sql)
+    # database auto_increment(id value) rearrange query
+    auto_increment_sql1 = "ALTER TABLE tellmeaboutme.list AUTO_INCREMENT=1"
+    db.execute(auto_increment_sql1)
+    auto_increment_sql2 = "SET @count = 0"
+    db.execute(auto_increment_sql2)
+    auto_increment_sql3 = "UPDATE tellmeaboutme.list SET id = @count:=@count+1"
+    db.execute(auto_increment_sql3)
+    db.commit()
+
+    return redirect(url_for('main.guestbook_list'))
+
+
+@main.route('/update', methods=["POST"])
+def update():
+
+    print(1)
+
+
+@main.route('/check_password', methods=['POST'])
+def divide_method():
+
+    print(1)
+    # if method_name == "delete":
+    #    return redirect(url_for('main.delete', id=id))
+    # else:
+    #   return redirect(url_for('main.updata', id=id))
+
+
+@main.route('/password', methods=["POST"])
+def password():
+
+    method_name = request.form['_method']
+    get_id = request.form['id']
+
+    return render_template('/checkpassword.html', method_name=method_name, get_id=get_id)
